7c1b8a501bcfa11c987ee110e1a3aca01ff70d02
Bodeode1 <bodeode2000@yahoo.com>
2023-06-26 13:40:25 +0000

    Set up import modules, define clear-terminal function and show_game_title function

--- run.py
+++ run.py
@@ -1,3 +1,35 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import time   
+from random import randint
+import os  
+import pyfiglet
+from colorama import init , Fore
+
+init(autoreset=True) #Initialize colorama
+
+def clear_terminal():
+    """
+    Clears the contents of the terminal screen.
+
+    """
+    os_name = os.name  #Retrieves the name of the operating system
+   
+    if os_name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def show_game_title():
+    """
+    This Function displays the game title and provides introductory text to the player
+
+    """
+
+    print(pyfiglet.figlet_format('PIRATE OF MARS'))
+    time.sleep(2)
+    print("You are going to destroy pirate aliens.")
+    print("Aliens now terrorize passengers embarking on interplanetary travel.")
+    print("You are part of the space force tasked with safeguarding space travel.")
+    print("Select any of the number below to proceed.")
+    print()
