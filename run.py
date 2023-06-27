235a88974fa7db577e8ea37c0680094f6dfc27f0
Bodeode1 <bodeode2000@yahoo.com>
2023-06-27 19:06:52 +0000

    Add function to collect input from player, then set variables for the game

--- run.py
+++ run.py
@@ -33,3 +33,47 @@ def show_game_title():
     print("You are part of the space force tasked with safeguarding space travel.")
     print("Select any of the number below to proceed.")
     print()
+
+def show_game_instructions():
+    """
+    This function displays the instructions of the game
+
+    """
+
+    print("To start the game, press y to start and n to stop.")
+    print("You can play the game 5 times, and you need to kill 3 aliens to win.")
+    print("You kill an alien when you fire your weapon at their hiding cell.")
+    print("The cells are numbered from 1 to 9(10 not inclusive).")
+    print("For example, if you need to kill an alien at cell 4, you will enter 4 when prompted")
+
+def collect_player_input(message:"str"):
+    """
+    This function collects input from a player or user.
+
+    Parameters:
+        message (str): The message to display as a prompt to the player/user.
+
+    Returns:
+        str: The value entered by the player/user.
+    """
+    
+    value_entered =input(message)
+    return value_entered
+
+
+# Define a global variable to keep track of the total number of plays
+global total_play
+
+#Initialize the total_play variable to 0
+total_play = 0
+
+scores = 0
+
+#Initialize an empty list to represent the gameboard
+gameboard = []
+
+#Set the maximum_trial variable to 5
+maximum_trial = 5
+
+global play_game
+play_game = True
